114af29d3f88d954cf74e33d7eeea4ac6bbf5432
Joao Siqueira <jvitorsiqueira99@gmail.com>
2026-08-06 22:22:29 -0300

    criar UsuarioRequest e UsuarioResponse

---
+++ app/src/main/java/com/joaosiqueira/app/dto/UsuarioRequest.java
@@ -0,0 +1,5 @@
+package com.joaosiqueira.app.dto;
+
+public record UsuarioRequest(String nome, String email, String senha) {
+
+}
---
+++ app/src/main/java/com/joaosiqueira/app/dto/UsuarioResponse.java
@@ -0,0 +1,4 @@
+package com.joaosiqueira.app.dto;
+
+public record UsuarioResponse(Long id, String nome, String email) {
+}
